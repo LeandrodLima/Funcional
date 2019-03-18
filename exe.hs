ee6a581a448fe3b0b5e69c4343d2216d50365948
LeandrodLima <leandrodsouto@gmail.com>
2019-03-18 15:32:40 -0300

    Update exe.hs

--- exe.hs
+++ exe.hs
@@ -6,5 +6,11 @@ pow x y = x**y
 {-
 fatorial :: Integer -> Integer  
 fatorial n = product [1..n] 
+  -- ou
+let fatorial n | n == 0 = 1 | otherwise = fatorial (n - 1) * n
+-- Ainda podemos fazer:
+
+let fatorial 0 = 1
+let fatorial n = fatorial (n - 1) * n
     
 -}

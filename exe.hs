a93f3716971a8f9fccefabf825a37158f1096320
Leandro Lima <leandro.lima@ccc.ufcg.edu.br>
2019-04-01 15:14:01 -0300

    lambda

--- exe.hs
+++ exe.hs
@@ -1,10 +1,30 @@
 {-xor a b  =  a=!b
-impl a b = 
+impl a b =
 
 pow x y = x**y
 
+
+fatorial :: Integer -> Integer
+fatorial n = product [1..n]
+  -- ou
+let fatorial n | n == 0 = 1 | otherwise = fatorial (n - 1) * n
+-- Ainda podemos fazer:
+
+let fatorial 0 = 1
+let fatorial n = fatorial (n - 1) * n
+-
+
+
+let primo n = if (fatores n) == [1,n] then True
+else False
+-}
+square = \x -> x*x
+--_______lambda ________________
+-- 1 pow x y
 {-
-fatorial :: Integer -> Integer  
-fatorial n = product [1..n] 
-    
+pow =  \x y -> case y of
+         1 -> x
+         0 -> 1
+         otherwise -> x*(pow x (y-1))
+pow =  \x y -> if y == 0 then 1 else x* (pow x (y-1))
 -}
